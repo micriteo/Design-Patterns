4145cf9b0915e32d29825688300efce6f1ebaad9
Arian Atapour <arian.atapour@student.nhlstenden.com>
2024-03-01 12:25:58 +0100

    Demo version of Factory

---
+++ Factory/Factory/Movie.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factory
+{
+    internal class Movie:Watchable
+    {
+        private String _name;
+        private String _description;
+        public Movie(string name, string description)
+        {
+            this.watchable(name, description);
+        }
+        public void watchable(string name, string description)
+        {
+            if (name == null || description == null)
+            {
+                throw new ArgumentException("Can not leave name null !");
+            }
+            else
+            {
+                this._name = name;
+                this._description = description;
+            }
+            //throw new NotImplementedException();
+        }
+
+        public String getName()
+        {
+            return this._name;
+        }
+
+        public String getDescription()
+        {
+            return this._description;
+        }
+    }
+}
---
+++ Factory/Factory/Show.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Core.AnimationMetrics;
+
+namespace Factory
+{
+    internal class Show : Watchable
+    {
+        private String _name;
+        private String _description;
+        public Show(string name, string description) { 
+             this.watchable(name, description);
+        }
+        public void watchable(string name, string description)
+        {
+            if (name == null || description == null)
+            {
+                throw new ArgumentException("Can not leave name null !");
+            }
+            else
+            {
+                this._name = name;
+                this._description = description;
+            }
+            //throw new NotImplementedException();
+        }
+
+        public String getName()
+        {
+            return this._name;
+        }
+
+        public String getDescription()
+        {
+            return this._description;
+        }
+
+
+    }
+}
